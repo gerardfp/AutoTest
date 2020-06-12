3fd46c8e0079db53d8ff0537f38cf9f6bd2ddc2b
gerard <gerardfp@gmail.com>
2020-06-12 10:55:16 +0200

    -

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -11,34 +11,25 @@ import java.util.Scanner;
 public class Main {
     private static MoodleAPI api;
 
-//    private static String token;
-//    private static String assignId;
-//    private static List<String> assignIds = new ArrayList<>();
-//    private static HashMap<String, String> id2username = new HashMap<>();
-
     public static void main(String[] args) {
-//        System.out.println("start");
         api =  new Retrofit.Builder()
                 .baseUrl("https://moodle.elpuig.xeill.net/")
                 .client(new OkHttpClient.Builder()
-                    .addInterceptor(new Interceptor() {
-                        @Override
-                        public Response intercept(Chain chain) throws IOException {
-                            Request original = chain.request();
-                            HttpUrl originalHttpUrl = original.url();
+                    .addInterceptor(chain -> {
+                        Request original = chain.request();
+                        HttpUrl originalHttpUrl = original.url();
 
-                            HttpUrl url = originalHttpUrl.newBuilder()
-                                    .addQueryParameter("moodlewsrestformat", "json")
-                                    .build();
+                        HttpUrl url = originalHttpUrl.newBuilder()
+                                .addQueryParameter("moodlewsrestformat", "json")
+                                .build();
 
-                            Request.Builder requestBuilder = original.newBuilder()
-                                    .url(url);
+                        Request.Builder requestBuilder = original.newBuilder()
+                                .url(url);
 
-                            Request request = requestBuilder.build();
+                        Request request = requestBuilder.build();
 
 //                            System.out.println(("INTERCEPTOR " + String.format("Sending request %s on %s%n%s", request.url(), chain.connection(), request.headers())));
-                            return chain.proceed(request);
-                        }
+                        return chain.proceed(request);
                     }).build())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
@@ -54,11 +45,7 @@ public class Main {
         String username = scanner.nextLine();
         System.out.println("Moodle password: ");
         String password = scanner.nextLine();
-
-//        System.out.println("login...");
         api.login(username, password).enqueue((Callback<Token>) response -> {
-//            System.out.println("TOKEN = " + response.token);
-
             getAssignments(response.token);
         });
     }
@@ -66,19 +53,11 @@ public class Main {
     private static void getAssignments(String token){
 
         try {
-            Courses courses = api.assignments(token).execute().body();
-
-            courses.courses.forEach(course -> {
+            api.assignments(token).execute().body().courses.forEach(course -> {
                 course.assignments.forEach(assignment -> {
                     try {
                         api.submissions(token, assignment.id).execute().body().assignments.forEach(assignment1 -> {
                             assignment.submissions = assignment1.submissions;
-
-//                            assignment1.submissions.forEach(submission -> {
-//                                if(submission.gradingstatus.equals("notgraded")){
-//                                    System.out.println("\t\thttps://moodle.elpuig.xeill.net/mod/assign/view.php?id=" + assignment.cmid +"&rownum=0&action=grader&userid="+ submission.userid);
-//                                }
-//                            });
                         });
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -118,29 +97,21 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-//        api.assignments(token).enqueue((Callback<Courses>) response -> {
-//            response.courses.forEach(c -> {
-//                c.assignments.forEach(a -> {
-//                    getSubmissions(token, a.id, a.cmid, c.fullname, a.name);
+//    private static void getSubmissions(String token, String assignId, String assignCmid, String courseName, String assignmentName){
+//        api.submissions(token, assignId).enqueue((Callback<Assignments>) response -> {
+//            System.out.println("\t" + courseName);
+//            System.out.println("\t\t" + assignmentName);
+//            response.assignments.forEach(a ->{
+//                a.submissions.forEach(s -> {
+//                    if(s.gradingstatus.equals("notgraded")){
+//                        System.out.println("https://moodle.elpuig.xeill.net/mod/assign/view.php?id=" + assignCmid +"&rownum=0&action=grader&userid="+ s.userid);
+//                    }
 //                });
 //            });
 //        });
-    }
-
-    private static void getSubmissions(String token, String assignId, String assignCmid, String courseName, String assignmentName){
-        api.submissions(token, assignId).enqueue((Callback<Assignments>) response -> {
-            System.out.println("\t" + courseName);
-            System.out.println("\t\t" + assignmentName);
-            response.assignments.forEach(a ->{
-                a.submissions.forEach(s -> {
-                    if(s.gradingstatus.equals("notgraded")){
-                        System.out.println("https://moodle.elpuig.xeill.net/mod/assign/view.php?id=" + assignCmid +"&rownum=0&action=grader&userid="+ s.userid);
-                    }
-                });
-            });
-        });
-    }
+//    }
 
 //    private static void getDiscussions(){
 //        api.discussions(token, Config.forumid).enqueue((com.example.Callback<com.example.Discusiones>) response -> {
